10dbfbdeb189cc57b66a808c3b3fba76e2f34490
Luciano De Gianni <43427391+lucianojd@users.noreply.github.com>
2018-10-18 20:56:38 -0700

    Created Board constructor and intialized Random object and Scanner object.

--- Board.java
+++ Board.java
@@ -1,4 +1,18 @@
+import java.util.Random
+import java.util.Scanner
+
 public class Board
 {
+	Scanner input = new Scanner(System.in);
+	Random 	value = new Random();
+	int[] 	code;
+	int attempts;
+	
+	Board()
+	{
+		code = int[4];
+		attempts = 12;
+	}
+	
 	
 }
