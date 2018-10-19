0420ab2838d5c234ecb6ade28e9962fddcde29d1
Luciano De Gianni <43427391+lucianojd@users.noreply.github.com>
2018-10-19 10:22:39 -0700

    Tried using Java documentation. Will look into it later.

--- BoardInterface.java
+++ BoardInterface.java
@@ -1,4 +1,14 @@
 public interface BoardInterface
 {
-
+/**
+* <h1>CodeBreaker</h1>
+* CodeBreaker is a single player version of the board 
+* game mastermind.
+* <p>
+* The class board contatains two constructors.
+* One is a blank constructor with no parameters. 
+* The default values are 4 entries and 12 attempts.
+* The custom game constructor takes two parameters:
+* Board(int entries, int attempts);
+*/
 }
