57fd18e80f9fef3077513592612e09f3b12d7d28
Luciano De Gianni <43427391+lucianojd@users.noreply.github.com>
2018-10-19 12:04:52 -0700

    Built the toString, and buildBoard methods in the Board class. Tested toString method, works correctly.

--- Board.java
+++ Board.java
@@ -1,18 +1,60 @@
-import java.util.Random
-import java.util.Scanner
+import java.util.Random;
+import java.util.Scanner;
 
 public class Board
 {
-	Scanner input = new Scanner(System.in);
-	Random 	value = new Random();
-	int[] 	code;
-	int attempts;
+	private Scanner input = new Scanner(System.in);
+	private Random 	value = new Random();
+	private int[] 	code;
+	private int attempts;
 	
 	Board()
 	{
-		code = int[4];
+		code = new int[4];
 		attempts = 12;
 	}
 	
+	Board(int entries, int attempts)
+	{
+		code = new int[entries];
+		this.attempts = attempts;
+	}
 	
+	public void newGame()
+	{
+		buildBoard();
+		int[] guess = new int[code.length];
+	}
+	
+	private boolean checkGuessSyntax(int[] guess)
+	{
+		return false;
+	}
+	
+	private boolean checkGuess(int[] guess)
+	{
+		return false;
+	}
+	
+	private void buildBoard()
+	{
+		for(int i = 0; i < code.length; i++)
+		{
+			code[i] = value.nextInt(10);
+		}
+	}
+	
+	public String toString()
+	{
+		StringBuilder codeReturn = new StringBuilder(code[0] + " ");
+		
+		for(int i = 1; i < code.length - 1; i++)
+		{
+			codeReturn.append(code[i] + " ");
+		}
+		
+		codeReturn.append(code[code.length - 1]);		
+		
+		return new String(codeReturn);
+	}
 }
--- BoardInterface.java
+++ BoardInterface.java
@@ -1,14 +1,12 @@
 public interface BoardInterface
 {
-/**
-* <h1>CodeBreaker</h1>
-* CodeBreaker is a single player version of the board 
-* game mastermind.
-* <p>
-* The class board contatains two constructors.
-* One is a blank constructor with no parameters. 
-* The default values are 4 entries and 12 attempts.
-* The custom game constructor takes two parameters:
-* Board(int entries, int attempts);
-*/
+	/*
+	* Constructors: Board(); , Board(int entries, int attempts);
+	* Default entries and attempts are 4 and 12 respectively.
+	*/
+	
+	/*
+	* Begins a new game based on the values passed by the constructor.
+	*/
+	public void newGame();
 }
