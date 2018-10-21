1afdcc3b1932e4f69ca7ddc4d7e962aa0f28ea70
Luciano De Gianni <43427391+lucianojd@users.noreply.github.com>
2018-10-21 14:38:02 -0700

    Added more comments and tried to fix the correct number wrong place counter. Next time try both an array that prevents checking spots tat are correct and values that have already been checked.

--- Board.java
+++ Board.java
@@ -22,8 +22,14 @@ public class Board
 	
 	public void newGame()
 	{
+	// Retrieves a board for the user.
 		buildBoard();
+		System.out.println(printCode(code));
 		
+	//	guess will hold the users guess as an integer array.
+	//	numGuess keeps track of the number of guesses the user has made.
+	//	guessInput holds the users input as a String.
+	//	temp holds a boolean value for checkGuessSyntax and checkGuess to reduce function calls and writing.
 		int[] guess = new int[code.length];
 		int numGuess = 0;
 		String guessInput;
@@ -31,9 +37,10 @@ public class Board
 		
 		System.out.println("You have " + attempts + " attempts to guess a " + code.length + " digit long code.");
 		
-		
+	//	Main game executes until the user has made their maximum number of guesses.
 		while(numGuess < attempts)
 		{	
+		//	Promtps the user for their guess and tells them how many attempts they have left.
 			System.out.printf("Enter your guess %11s", "(" + numGuess + "/" + attempts + ") : ");
 			guessInput = input.nextLine();
 			temp = checkGuessSyntax(guessInput);
@@ -55,29 +62,38 @@ public class Board
 				
 				if(temp)
 				{
-					System.out.println("You've cracked the code!");
+				//	If the user guessed the code.
 					numGuess = attempts;
+					System.out.println("You've cracked the code!");
 				}
 				else if(!temp && numGuess == attempts)
 				{
+				// If the user ran out of attempts.
 					System.out.println("Game Over.");
+					System.out.println("The code was: " + printCode(code));
 				}
 			} 
 			else if(!temp)
 			{
+			// If the user puts in an invalid code.
 				System.out.println("Not a valid code.");
 			}
 			
 		}
-		
+		System.out.println("Type \"q\" to quit.");
+		input.next();
 	}
 	
 	private boolean checkGuessSyntax(String guess)
 	{
+	//	If the user enters a string with a length greater or less than
+	//	the code length then the input must be invalid.
 		if(guess.length() == code.length)
 		{
 			for(int i = 0; i < code.length; i++)
 			{
+			// If the parseInt method throw a NumberFormatException then the
+			//	user must have entered a code containing non integer values.
 				try
 				{
 					Integer.parseInt(guess);
@@ -95,35 +111,56 @@ public class Board
 	
 	private boolean checkGuess(int[] guess)
 	{
+	//	rightNum is a counter for the amount of correct number.
+	//	rightNumPlace is a counter for the correct amount of numbers in the 
+	//	correct place.
+	//	checkedValues conatins the numbers already present in the code.
 		int rightNum = 0;
 		int rightNumPlace = 0;
+		int[] checkedLocations = new int[code.length];
+		int[] checkedValues = new int[code.length];
+		
+	//	Initializes array so that there are no accidental matches
+	//	when comparing values.
+		for(int i = 0; i < code.length; i++)
+		{
+			checkedLocations[i] = -1;
+		}
 		
 		for(int i = 0; i < code.length; i++)
 		{
-			for(int j = 0; j < code.length; j++)
-			{
-				if(code[i] == guess[j] && i == j)
+				if(code[i] == guess[i])
 				{
+				//	Increment rightNumPlace if the value is equal to a value
+				//	in the coorresponding location. Add that i value to checkedValues
+				//	as it doesn't need to be checked again.
 					rightNumPlace++;
+					checkedLocations[i] = i;
 				}
-				else if(code[i] == guess[j])
-				{
-					rightNum++;
-				}
+		}
+		
+		for(int i = 0; i < code.length; i++)
+		{
+			if(i != checkedLocations[i])
+			{
+			
 			}
 		}
+		
+		
 		
 		if(rightNumPlace == code.length) 
 		{
 			return true;
 		}
 		
-		System.out.println("Correct numbers: " + rightNum);
-		System.out.println("Correct places: " + rightNumPlace);
+		System.out.println("Correct number wrong place: " + rightNum);
+		System.out.println("Correct place right number: " + rightNumPlace);
 		
 		return false;
 	}
 	
+//	Builds a board for the user using the Random object methods.
 	private void buildBoard()
 	{
 		for(int i = 0; i < code.length; i++)
@@ -131,7 +168,8 @@ public class Board
 			code[i] = value.nextInt(10);
 		}
 	}
-	
+
+// Prints a formatted version of the code.
 	private String printCode(int[] code)
 	{
 		StringBuilder codeReturn = new StringBuilder(code[0] + " ");
